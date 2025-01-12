74ab212d0121510000553b0365816374bc4d6e06
Loic Chevalier <loicchechevalier@gmail.com>
2025-01-12 14:46:40 +0100

    TP1 14

--- app/src/main/java/com/loicche/todo/list/Task.kt
+++ app/src/main/java/com/loicche/todo/list/Task.kt
@@ -1,5 +1,5 @@
 package com.loicche.todo.list
 
-data class Task(val id: String, val title: String, val description: String = "Yeah... Busy, you know what I mean") {
+data class Task(val id: String, val title: String, val description: String = "Gotta do something") {
 
 }
--- app/src/main/java/com/loicche/todo/list/TaskListFragment.kt
+++ app/src/main/java/com/loicche/todo/list/TaskListFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.loicche.todo.R
+import java.util.UUID
 
 
 class TaskListFragment : Fragment() {
@@ -20,7 +22,7 @@ class TaskListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
-        adapter.currentList = taskList
+        adapter.submitList(taskList)
         return rootView
     }
 
@@ -28,5 +30,14 @@ class TaskListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView = view.findViewById<RecyclerView>(R.id.task_view_list)
         recyclerView.adapter = this.adapter
+
+        val button = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        button.setOnClickListener {
+            val newTask = Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}")
+            taskList = taskList + newTask
+
+            adapter.submitList(taskList)
+        }
+
     }
 }
